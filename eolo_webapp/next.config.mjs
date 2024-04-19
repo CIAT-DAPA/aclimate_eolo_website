ce03adc891328f7b08ffb071fff9c6a9aa56b291
santiagoC <s.calderon@cgiar.org>
2024-04-19 11:23:42 -0600

    next config

--- eolo_webapp/next.config.mjs
+++ eolo_webapp/next.config.mjs
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-basePath: "/aclimate_eolo_website",
+  basePath: "/aclimate_eolo_website",
+  assetPrefix: "/nextjs-page",
   output: "export",
   reactStrictMode: true,
 };
